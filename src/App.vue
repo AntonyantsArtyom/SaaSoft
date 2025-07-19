c6e6e56c9b4cf7ca7ce2ecadf2401c89a9608cb9
Артем Антонянц <aa.artem.0303@mail.ru>
2025-07-19 05:38:40 +0500

    chore: delete help message

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import RecordsTable from "./entities/Records/RecordsTable.vue";
 import AddRecordForm from "./features/AddRecordForm.vue";
-import HelpMessage from "./shared/HelpMessage.vue";
 import { NConfigProvider } from "naive-ui";
 </script>
 
 <template>
   <n-config-provider>
     <add-record-form />
-    <help-message text='Для указания нескольких меток используйте разделитель ";"' />
     <records-table />
   </n-config-provider>
 </template>
