37186fd35eb5c1cb8d137b7610905df63b6084e9
Артем Антонянц <aa.artem.0303@mail.ru>
2025-07-20 00:10:32 +0500

    chore: remove initial records

--- src/entities/Records/RecordsStore.ts
+++ src/entities/Records/RecordsStore.ts
@@ -3,22 +3,7 @@ import { RecordDataTypes, type RecordData, type RecordsDataState } from "./Recor
 
 export const useRecordsStore = defineStore("RecordsStore", {
   state: (): RecordsDataState => ({
-    records: [
-      {
-        id: "1",
-        type: RecordDataTypes.LOCAL,
-        login: "user1",
-        password: "password1",
-        tags: ["tag1", "tag2"],
-      },
-      {
-        id: "2",
-        type: RecordDataTypes.LDAP,
-        login: "user2",
-        password: "password2",
-        tags: ["tag3", "tag4"],
-      },
-    ],
+    records: [],
   }),
 
   actions: {
